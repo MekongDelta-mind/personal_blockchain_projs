31d53d68b2cf7730876912b654d4f1f5c15a8d4b
P K Nayak <47595700+MekongDelta-mind@users.noreply.github.com>
2024-08-31 11:26:27 +0530

    completing the basic types

--- updraft_cyfrin/PatrickCollins/SimpleStorageSC/SimpleStorage.sol
+++ updraft_cyfrin/PatrickCollins/SimpleStorageSC/SimpleStorage.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 // the above line is to add a liscence for the this("SimpleStorage.sol") which is like opne source liscence
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity 0.8.18;
+// pragma solidity >=0.6.0 <0.9.0; // every compiler above version 0.6.0 and below 0.9.0
 //pragma solidity >=0.6.0 ; // every compiler above version 0.6.0
 //pragma solidity ^0.6.0 ; // every compiler above version 0.6.0 below 0.7 and above 0.6
 
@@ -9,37 +10,38 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
 
-    // this will initialize to 0 like Java
-    uint256 favoriteNumber; // un-signed integer of 256 bits
-/*  Other data types used in solidity
+//     // this will initialize to 0 like Java
+//     uint256 favoriteNumber; // un-signed integer of 256 bits
+
+    //   Other data types used in solidity
     bool favoriteBoolean = true ;
-    string favoriteString = 'string' ;
+    string favoriteString = 'ampn' ;
     int256 favoriteInt = -5 ; // this can contain unsigned integer
-    address favoriteAddress = 0X16343943169463169fe434136f641625843 ; // this is a new type of data type 
-                                                                    which stores the address of the blockchain or the account in which you are trying to deal with
-*/
-    // This is a comment!
-    struct People {
-        uint256 favoriteNumber;
-        string name;
-    }
+    // address favoriteAddress = 0X16343943169463169fe434136f641625843 ; // this is a new type of data type 
+                                                                    // which stores the address of the blockchain or the account in which you are trying to deal with
 
-    People[] public people; // dynmaic array
-//    People[3] public people; // fixed array of size 3
+//     // This is a comment!
+//     struct People {
+//         uint256 favoriteNumber;
+//         string name;
+//     }
+
+//     People[] public people; // dynmaic array
+// //    People[3] public people; // fixed array of size 3
 
 
-    mapping(string => uint256) public nameToFavoriteNumber; //key -> string and value -> unit256
+//     mapping(string => uint256) public nameToFavoriteNumber; //key -> string and value -> unit256
 
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-    }
+//     function store(uint256 _favoriteNumber) public {
+//         favoriteNumber = _favoriteNumber;
+//     }
     
-    function retrieve() public view returns (uint256){
-        return favoriteNumber;
-    }
+//     function retrieve() public view returns (uint256){
+//         return favoriteNumber;
+//     }
 
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
-    }
+//     function addPerson(string memory _name, uint256 _favoriteNumber) public {
+//         people.push(People(_favoriteNumber, _name));
+//         nameToFavoriteNumber[_name] = _favoriteNumber;
+//     }
 }
